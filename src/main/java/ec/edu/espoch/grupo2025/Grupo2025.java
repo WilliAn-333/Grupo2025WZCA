4b2808f94929183027bb72fefa975c21000482e6
anthonypulupa <anthonypulupa2@gmail.com>
2025-01-19 12:13:56 -0500

    Actualizar

--- src/main/java/ec/edu/espoch/grupo2025/Grupo2025.java
+++ src/main/java/ec/edu/espoch/grupo2025/Grupo2025.java
@@ -13,7 +13,6 @@ public class Grupo2025 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-         Interfas1 objInterfas=new Interfas1();
-       objInterfas.setVisible(true);
+         
     }
 }
